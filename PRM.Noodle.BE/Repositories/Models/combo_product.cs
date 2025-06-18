89b88a884d7358c5003bdf8d5a8038298ff0fe7c
hieuvau198 <nhatduongfpt2000@gmail.com>
2025-06-18 20:12:08 +0700

    Add Scaffold

---
+++ PRM.Noodle.BE/Repositories/Models/combo_product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repositories.Models;
+
+public partial class combo_product
+{
+    public int combo_product_id { get; set; }
+
+    public int combo_id { get; set; }
+
+    public int product_id { get; set; }
+
+    public int? quantity { get; set; }
+
+    public virtual combo combo { get; set; } = null!;
+
+    public virtual product product { get; set; } = null!;
+}
---
+++ PRM.Noodle.BE/Repositories/Models/order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repositories.Models;
+
+public partial class order
+{
+    public int order_id { get; set; }
+
+    public int user_id { get; set; }
+
+    public string? order_status { get; set; }
+
+    public decimal total_amount { get; set; }
+
+    public string? payment_method { get; set; }
+
+    public string? payment_status { get; set; }
+
+    public string? delivery_address { get; set; }
+
+    public string? notes { get; set; }
+
+    public DateTime? order_date { get; set; }
+
+    public DateTime? confirmed_at { get; set; }
+
+    public DateTime? completed_at { get; set; }
+
+    public virtual ICollection<order_combo> order_combos { get; set; } = new List<order_combo>();
+
+    public virtual ICollection<order_item> order_items { get; set; } = new List<order_item>();
+
+    public virtual user user { get; set; } = null!;
+}
