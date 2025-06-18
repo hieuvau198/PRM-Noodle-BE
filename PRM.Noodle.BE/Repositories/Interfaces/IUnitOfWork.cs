36bda0e10583f361cbf1f387015cd5ee29ce250b
hieuvau198 <nhatduongfpt2000@gmail.com>
2025-06-19 00:33:41 +0700

    update repo

---
+++ PRM.Noodle.BE/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Repositories.Interfaces
+//{
+//    public interface IUnitOfWork : IDisposable
+//    {
+//        IUserRepository Users { get; }
+//        IProductRepository Products { get; }
+//        IGenericRepository<Topping> Toppings { get; }
+//        IGenericRepository<Combo> Combos { get; }
+//        IOrderRepository Orders { get; }
+//        IGenericRepository<OrderItem> OrderItems { get; }
+//        IGenericRepository<OrderCombo> OrderCombos { get; }
+//        IGenericRepository<DailyRevenue> DailyRevenues { get; }
+
+//        int Complete();
+//        Task<int> CompleteAsync();
+//        Task BeginTransactionAsync();
+//        Task CommitTransactionAsync();
+//        Task RollbackTransactionAsync();
+//    }
+//}
---
+++ PRM.Noodle.BE/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿//using Repositories.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Repositories.Interfaces
+//{
+//    public interface IUserRepository : IGenericRepository<User>
+//    {
+//        Task<User> GetByUsernameAsync(string username);
+//        Task<User> GetByEmailAsync(string email);
+//        Task<bool> IsUsernameExistsAsync(string username);
+//        Task<bool> IsEmailExistsAsync(string email);
+//    }
+//}
