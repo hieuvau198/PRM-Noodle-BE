a07eb343abdbf0daad709ba0d263b75936205647
Pham-Dang-Khoi <phamdangkhoir@gmail.com>
2025-06-21 08:25:34 +0700

    edit domain FE

--- PRM.Noodle.BE/API/Program.cs
+++ PRM.Noodle.BE/API/Program.cs
@@ -49,7 +49,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin", policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "https://localhost:3001") // Add your frontend URLs
+        policy.WithOrigins("http://localhost:3000", "https://localhost:3001", "http://localhost:5173", "http://localhost:5174") // Add your frontend URLs
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
