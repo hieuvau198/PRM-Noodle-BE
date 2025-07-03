7af686aadad6a20d3109196787dc7d657f97005b
PDK-23 <phamdangkhoir@gmail.com>
2025-07-03 08:50:08 +0700

    edit name space

--- PRM.Noodle.BE/Services/Interfaces/IToppingService.cs
+++ PRM.Noodle.BE/Services/Interfaces/IToppingService.cs
@@ -1,12 +1,15 @@
 ﻿using Services.DTOs.Topping;
 
-public interface IToppingService
+namespace Services.Interfaces
 {
-    Task<IEnumerable<ToppingDto>> GetAllAsync();
-    Task<ToppingDto> GetByIdAsync(int id);
-    Task<ToppingDto> CreateAsync(CreateToppingDto dto);
-    Task<ToppingDto> UpdateAsync(int id, UpdateToppingDto dto);
-    Task<bool> DeleteAsync(int id);
-    Task<IEnumerable<ToppingDto>> GetAvailableAsync();
-    Task<bool> PatchIsAvailableAsync(int id, bool isAvailable);
+    public interface IToppingService
+    {
+        Task<IEnumerable<ToppingDto>> GetAllAsync();
+        Task<ToppingDto> GetByIdAsync(int id);
+        Task<ToppingDto> CreateAsync(CreateToppingDto dto);
+        Task<ToppingDto> UpdateAsync(int id, UpdateToppingDto dto);
+        Task<bool> DeleteAsync(int id);
+        Task<IEnumerable<ToppingDto>> GetAvailableAsync();
+        Task<bool> PatchIsAvailableAsync(int id, bool isAvailable);
+    }
 }
---
+++ PRM.Noodle.BE/Services/Mappings/ToppingMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Repositories.Models;
+using Services.DTOs.Topping;
+
+namespace Services.Mappings
+{
+    public class ToppingMappingProfile : Profile
+    {
+        public ToppingMappingProfile()
+        {
+            CreateMap<Topping, ToppingDto>().ReverseMap();
+            CreateMap<CreateToppingDto, Topping>();
+            CreateMap<UpdateToppingDto, Topping>();
+        }
+    }
+}
